d21e12d3da86dab0f2059ac86405a4df17e22e4e
Carlos Silva <brsilvacarlos@gmail.com>
2025-03-21 22:45:05 -0300

    Trabalhando com mocks

--- __tests__/application/TransferenciaServicoMock.test.ts
+++ __tests__/application/TransferenciaServicoMock.test.ts
@@ -2,16 +2,22 @@ import { TransferenciaDTO } from "../../src/application/TransferenciaDTO";
 import { TransferenciaServico } from "../../src/application/TransferenciaServico";
 import { Conta } from "../../src/model/Conta";
 import { Repositorio } from "../../src/model/contract/Repositorio";
-import { MemoriaContaRepositorio } from "../fake/MemoriaContaRepositorio";
 
 describe('Transferência Serviço', () => {
     test('Transferir com sucesso', async () => {
-        const repositorio: Repositorio<string, Conta> = criarContaRepositorio();
+        const { repositorio, contaOrigem, contaDestino } = criarMock();
 
         const dto: TransferenciaDTO = new TransferenciaDTO('123456', '654321', 100);
 
         const transferenciaServico: TransferenciaServico = new TransferenciaServico(repositorio);
         const recibo: string = await transferenciaServico.transferir(dto);
+       
+        expect(repositorio.buscar).toBeCalledWith("123456");
+        expect(repositorio.buscar).toBeCalledWith("654321");
+        expect(repositorio.buscar).toBeCalledTimes(2);
+        expect(repositorio.adicionar).toBeCalledWith(contaOrigem);
+        expect(repositorio.adicionar).toBeCalledWith(contaDestino);
+        expect(repositorio.adicionar).toBeCalledTimes(2);
 
         const contaOrigemRepositorio = await repositorio.buscar('123456');
         const contaDestinoRepositorio = await repositorio.buscar('654321');
@@ -19,36 +25,58 @@ describe('Transferência Serviço', () => {
         expect(contaOrigemRepositorio!.saldo).toBe(4900);
         expect(contaDestinoRepositorio!.saldo).toBe(5100);
         expect(recibo.length).toBe(6)
+
     })
 
+
     test("conta de origem não encontrada", async () => {
-        const repositorio: Repositorio<string, Conta> = criarContaRepositorio();
+        const { repositorio } = criarMock();
     
         const transferenciaServico: TransferenciaServico = new TransferenciaServico(repositorio);
-    
         const dto: TransferenciaDTO = new TransferenciaDTO("111111", "654321", 100.0);
     
         await expect(transferenciaServico.transferir(dto)).rejects.toEqual(Error("conta de origem não encontrada"));
+        expect(repositorio.buscar).toBeCalledWith("111111");
+        expect(repositorio.buscar).toBeCalledWith("654321");
+        expect(repositorio.buscar).toBeCalledTimes(2);
+        expect(repositorio.adicionar).not.toBeCalled();
     });
 
+
     test("conta de destino não encontrada", async () => {
-        const repositorio: Repositorio<string, Conta> = criarContaRepositorio();
+        const { repositorio } = criarMock();
     
         const transferenciaServico: TransferenciaServico = new TransferenciaServico(repositorio);
-    
         const dto: TransferenciaDTO = new TransferenciaDTO("123456", "222222", 100.0);
     
         await expect(transferenciaServico.transferir(dto)).rejects.toEqual(Error("conta de destino não encontrada"));
-
+        expect(repositorio.buscar).toBeCalledWith("123456");
+        expect(repositorio.buscar).toBeCalledWith("222222");
+        expect(repositorio.buscar).toBeCalledTimes(2);
+        expect(repositorio.adicionar).not.toBeCalled();
     });
+
+    
 })
 
-
-function criarContaRepositorio(): Repositorio<string, Conta> {
-    const repositorio: Repositorio<string, Conta> = new MemoriaContaRepositorio();
+function criarMock() {
+    const dicionario: Map<string, Conta> = new Map<string, Conta>();
     const contaOrigem: Conta = new Conta("123456", 5000);
     const contaDestino: Conta = new Conta("654321", 5000);
-    repositorio.adicionar(contaOrigem);
-    repositorio.adicionar(contaDestino);
-    return repositorio;
+    dicionario.set(contaOrigem.numero, contaOrigem);
+    dicionario.set(contaDestino.numero, contaDestino);
+
+    const repositorio: jest.Mocked<Repositorio<string, Conta>> = {
+        adicionar: jest.fn((entidade: Conta) => {
+            dicionario.set(entidade.numero, entidade);
+        }),
+        buscar: jest.fn((numero: string) => {
+            const promise = new Promise<Conta | undefined>((resolve, reject) => {
+                resolve(dicionario.get(numero));
+            });
+            return promise;
+        })
+    };
+
+    return { repositorio, contaOrigem, contaDestino };
 }
